8dd90c52ec1ce3b6a124edd386deb268a10b13f4
Kaytster <ktsharps@gmail.com>
2025-02-06 14:18:26 +0000

    Landing Page

---
+++ src/app/header.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+const Header = () => {
+    return (
+      <header id="nav">
+        <h1 id="head1">LOGO</h1>
+        <h1 id="head2">My Task Lists</h1>
+        <h1 id="head3">My Groups</h1>
+        <h1 id="head4">Create</h1>
+        <h1 id="head5">Account</h1>
+        <h1 id="head6">Settings</h1>
+      </header>
+    );
+  };
+  export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,26 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./header";
+import './globals.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+    <html>
+      <Header/>
+      <main>
+        <br/>
+        <br/>
+        <section className="intro">
+          <h1>Welcome to Jello</h1>
+          <p>With our Task Manager, you can easily keep your tasks organized with a list 
+          and share your tasks with others to work together.</p>
+          <p>With a Group Admin account, you can create groups for maximum collaboration.</p>
+          <p>To get started:</p>
+          <div id="introbuttons">
+            <p>Sign Up</p>
+            <p id="brk"></p>
+            <p>Login</p>
+          </div>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </html>
   );
 }
