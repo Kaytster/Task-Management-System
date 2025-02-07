d8a9a97c6e4d786931622f5ec3f0e5ee18c424d1
Kaytster <ktsharps@gmail.com>
2025-02-07 13:30:41 +0000

    Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,30 @@
 import Header from "./header";
-import './globals.css';
+import './global.css';
 
 export default function Home() {
   return (
     <html>
-      <Header/>
-      <main>
-        <br/>
-        <br/>
-        <section className="intro">
-          <h1>Welcome to Jello</h1>
-          <p>With our Task Manager, you can easily keep your tasks organized with a list 
-          and share your tasks with others to work together.</p>
-          <p>With a Group Admin account, you can create groups for maximum collaboration.</p>
-          <p>To get started:</p>
-          <div id="introbuttons">
-            <p>Sign Up</p>
-            <p id="brk"></p>
-            <p>Login</p>
-          </div>
-        </section>
-      </main>
+      <head>
+      </head>
+      <body>
+        <Header />
+        <main>
+          <br/>
+          <br/>
+          <section className="intro">
+            <h1>Welcome to Jello</h1>
+            <p>With our Task Manager, you can easily keep your tasks organized with a list 
+            and share your tasks with others to work together.</p>
+            <p>With a Group Admin account, you can create groups for maximum collaboration.</p>
+            <p>To get started:</p>
+            <div id="introbuttons">
+              <p>Sign Up</p>
+              <p id="brk"></p>
+              <p>Login</p>
+            </div>
+          </section>
+        </main>
+      </body>
     </html>
   );
 }
---
+++ src/app/signup/page.js
@@ -0,0 +1,12 @@
+
+export default function Signup() {
+    return (
+        <html>
+            <body>
+                <main>
+                    <h1>Jello</h1>
+                </main>
+            </body>
+        </html>
+    );
+}
