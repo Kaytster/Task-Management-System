27a6dfff4676e1dcc13d6e9921100bedc4082419
Kaytster <ktsharps@gmail.com>
2025-02-14 13:11:52 +0000

    create group

--- src/app/creategroup/page.js
+++ src/app/creategroup/page.js
@@ -1,9 +1,9 @@
 import Header from "../header";
 import 'bootstrap/dist/css/bootstrap.css'
 import '../globals.css';
-import '../styles/createlist.css'
+import '../styles/creategroup.css'
 
-export default function CreateList() {
+export default function CreateGroup() {
     return (
       <html>
         <head>
@@ -19,6 +19,19 @@ export default function CreateList() {
                     <div class="mb-3">
                         <label class="form-label"><b>Name</b></label>
                         <input type="listname" class="form-control" id="InputListName"/>
+                    </div>
+                    <div class="mb-3">
+                      <h3>Users</h3>
+                        <label class="form-label"><b>First Name</b></label>
+                        <input type="firstname" class="form-control" id="InputFirstName"/>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label"><b>Last Name</b></label>
+                        <input type="lastname" class="form-control" id="InputLastName"/>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label"><b>User ID</b></label>
+                        <input type="userid" class="form-control" id="InputUserID"/>
                     </div>
                     <button type="submit" class="btn-primary">Create</button> 
                 </form>
